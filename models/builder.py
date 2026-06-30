d64dece1f8771c99c9565a3776a8b57dcb6b638a
nnpuyen <nuyencrkh@gmail.com>
2026-07-01 00:25:05 +0700

    update transerr

--- models/builder.py
+++ models/builder.py
@@ -73,7 +73,7 @@ def _config_path(args, name: str, legacy_name: str = '') -> str:
 _INDEX_KGE_MODELS = frozenset({
 	'distmult', 'distmult-au', 'distmult-adversarial', 'distmult-adversarial-au',
 	'complex', 'complex-au', 'dabr', 'dabr-au', 'rotate', 'rotate-au', 'protate', 'protate-au',
-	'transe', 'transe-au', 'transerr',
+	'transe', 'transe-au', 'transerr', 'transerr-au',
 })
 
 
